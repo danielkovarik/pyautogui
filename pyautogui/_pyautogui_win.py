f2d8399d7c3404ef0aee9231272555db153b07f7
Daniel Kovarik <dan@bohemian.ai>
2019-07-18 16:01:00 +0200

    fixed shift+arrow bug, changed import for internal use

--- pyautogui/_pyautogui_win.py
+++ pyautogui/_pyautogui_win.py
@@ -4,8 +4,8 @@
 
 import ctypes
 import ctypes.wintypes
-import pyautogui
-from pyautogui import LEFT, MIDDLE, RIGHT
+from pyautogui import pyautogui
+from pyautogui.pyautogui import LEFT, MIDDLE, RIGHT
 
 import sys
 if sys.platform !=  'win32':
@@ -303,7 +303,12 @@ def _keyDown(key):
         (mods & 1 or needsShift, 0x10)]: #HANKAKU not suported! mods & 8
         if apply_mod:
             ctypes.windll.user32.keybd_event(vk_mod, 0, 0, 0) #
-    ctypes.windll.user32.keybd_event(vkCode, 0, 0, 0)
+    if keyboardMapping[key] == 0x25 or keyboardMapping[key] == 0x26 or keyboardMapping[key] == 0x27 or keyboardMapping[
+        key] == 0x28:
+        KEYEVENTF_EXTENDEDKEY = 1
+    else:
+        KEYEVENTF_EXTENDEDKEY = 0
+    ctypes.windll.user32.keybd_event(vkCode, 0, KEYEVENTF_EXTENDEDKEY, 0)
     for apply_mod, vk_mod in [(mods & 1 or needsShift, 0x10), (mods & 2, 0x11),
         (mods & 4, 0x12)]: #HANKAKU not suported! mods & 8
         if apply_mod:
